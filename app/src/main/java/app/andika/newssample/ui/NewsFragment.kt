f93e19e885556e925f78559540e4a4893ad6240d
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-11 19:31:37 +0700

    change view class name, navigation name, string name

---
+++ app/src/main/java/app/andika/newssample/ui/NewsFragment.kt
@@ -0,0 +1,23 @@
+package app.andika.newssample.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import app.andika.newssample.R
+import dagger.hilt.android.AndroidEntryPoint
+
+//@AndroidEntryPoint
+//class NewsFragment : BaseFragment() {
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+//    }
+//}
