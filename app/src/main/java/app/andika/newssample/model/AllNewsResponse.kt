82ca6ef032cce54934b55cc02d176a7d36e464f7
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-11 21:24:03 +0700

    make model parcelable

--- app/src/main/java/app/andika/newssample/model/AllNewsResponse.kt
+++ app/src/main/java/app/andika/newssample/model/AllNewsResponse.kt
@@ -1,7 +1,36 @@
 package app.andika.newssample.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class AllNewsResponse(
     val articles: List<Article>,
-    val status: String,
+    val status: String?,
     val totalResults: Int
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        TODO("articles"),
+        parcel.readString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(status)
+        parcel.writeInt(totalResults)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<AllNewsResponse> {
+        override fun createFromParcel(parcel: Parcel): AllNewsResponse {
+            return AllNewsResponse(parcel)
+        }
+
+        override fun newArray(size: Int): Array<AllNewsResponse?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/app/andika/newssample/model/Source.kt
+++ app/src/main/java/app/andika/newssample/model/Source.kt
@@ -1,6 +1,33 @@
 package app.andika.newssample.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Source(
     val id: Any,
-    val name: String
-)
+    val name: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        TODO("id"),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Source> {
+        override fun createFromParcel(parcel: Parcel): Source {
+            return Source(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Source?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
