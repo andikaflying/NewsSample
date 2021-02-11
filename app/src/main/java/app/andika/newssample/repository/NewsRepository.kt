b36875b2bf369db662ef810c7288b97bd6abc6d9
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-11 17:34:10 +0700

    setup ViewModel and Repository with Hilt

--- app/src/main/java/app/andika/newssample/repository/NewsRepository.kt
+++ app/src/main/java/app/andika/newssample/repository/NewsRepository.kt
@@ -1,4 +1,13 @@
 package app.andika.newssample.repository
 
-class NewsRepository {
+import android.content.Context
+import app.andika.newssample.core.api.APIList
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class NewsRepository @Inject constructor(
+    @ApplicationContext var context: Context,
+    val apiList: APIList
+) {
+
 }
--- app/src/main/java/app/andika/newssample/viewmodel/NewsViewModel.kt
+++ app/src/main/java/app/andika/newssample/viewmodel/NewsViewModel.kt
@@ -1,4 +1,17 @@
 package app.andika.newssample.viewmodel
 
-class NewsViewModel {
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import app.andika.newssample.repository.NewsRepository
+
+class NewsViewModel @ViewModelInject public constructor(
+    private val newsRepository: NewsRepository,
+    @Assisted private val savedStateHandle: SavedStateHandle
+)
+    : ViewModel(), LifecycleObserver {
+
+    private val TAG = NewsViewModel::class.java.name
 }
