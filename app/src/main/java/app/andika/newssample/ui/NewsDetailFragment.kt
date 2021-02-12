79c4c6667ec69705aa9b262167ec3461116bc348
Andika Kurniawan <44762285+andikaflying@users.noreply.github.com>
2021-02-12 20:35:36 +0700

    add slide animation

--- app/src/main/java/app/andika/newssample/ui/NewsDetailFragment.kt
+++ app/src/main/java/app/andika/newssample/ui/NewsDetailFragment.kt
@@ -3,10 +3,13 @@ package app.andika.newssample.ui
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import app.andika.newssample.R
 import app.andika.newssample.databinding.FragmentNewsBinding
 import app.andika.newssample.databinding.FragmentNewsDetailBinding
 import app.andika.newssample.model.Article
+import app.andika.newssample.utilities.touch.OnSwipeTouchListener
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +23,7 @@ class NewsDetailFragment : BaseFragment<FragmentNewsDetailBinding>()  {
 
         var selectedArticle = NewsDetailFragmentArgs.fromBundle(requireArguments()).selectedArticle
         displayDetail(selectedArticle)
+        setSwipeListener(view)
     }
 
     override fun getLayoutRes(): Int {
@@ -34,5 +38,36 @@ class NewsDetailFragment : BaseFragment<FragmentNewsDetailBinding>()  {
             .load(selectedArticle.urlToImage)
             .placeholder(R.drawable.ic_placeholder)
             .into(binding.ivNews)
+    }
+
+    fun setSwipeListener(view: View) {
+        view.setOnTouchListener(object : OnSwipeTouchListener(context) {
+            override fun onSwipeRight() {
+                goToOtherPage("right")
+                Toast.makeText(requireActivity(), "right", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onSwipeLeft() {
+                goToOtherPage("left")
+                Toast.makeText(requireActivity(), "left", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onSwipeTop() {
+                Toast.makeText(requireActivity(), "top", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onSwipeBottom() {
+                Toast.makeText(requireActivity(), "bottom", Toast.LENGTH_SHORT).show();
+            }
+        })
+    }
+
+    fun goToOtherPage(side : String) {
+        val fragment = NewsDetailFragment()
+        val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
+        transaction.replace(R.id.nav_host_fragment, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
